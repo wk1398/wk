e597b9376ae8ce53cc3edf757bfa03bf50e06173
wk1398 <1577328184@qq.com>
2019-04-14 19:58:51 +0800

    first commit

---
+++ src/main/java/GitTest.java
@@ -0,0 +1,21 @@
+/**
+ * Copyright (C), 2015-2019, XXX有限公司
+ * FileName: GitTest
+ * Author:   Admin
+ * Date:     2019/4/14 18:46
+ * Description:
+ * History:
+ * <author>          <time>          <version>          <desc>
+ * 作者姓名           修改时间           版本号              描述
+ */
+
+/**
+ * 〈一句话功能简述〉<br> 
+ * 〈〉
+ *
+ * @author Admin
+ * @create 2019/4/14
+ * @since 1.0.0
+ */
+public class GitTest {
+}
